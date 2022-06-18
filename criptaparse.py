03053cd472b10e967fe2ea6505a17dff785d7007
ARN1DIS <rybakalesha2007@gmail.com>
2022-06-18 09:37:02 +0300

    Update criptaparse.py

--- criptaparse.py
+++ criptaparse.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup as BS
 
 url = 'https://coinmarketcap.com'
 cripta_list = []
-up_or_down = None
+
 def parse(url):
     global up_or_down
     responce = requests.get(url)
@@ -23,19 +23,13 @@ def parse(url):
             all_cmc_link = data.find_all('a', {'class':'cmc-link'})
             price_long = all_cmc_link[1]
             price = price_long.find('span').text
-            #up/down per 24 hours in percent
-            value = data.find('span', {'class':"sc-15yy2pl-0 hzgCfk"}).text
-            #mark up or down
-            check = data.find('span', {'class':"icon-Caret-up"})
-            if(check != None):
-                up_or_down = "+"
-            else:
-                up_or_down = "-"
-            print(f"{name} цена: {price} возросла/упала = {up_or_down}{value}")
+            print(f"{name} цена: {price}")
             count_2 += 1
         except:
             data = cripta_list[count_2]
+            #name of cryptocurrency
             name = data.find('a', {'class':"cmc-link"}).text 
+            #price of cryptocurrency
             all_span = data.find_all('span')
             price = all_span[5].text
             print(f"{name} цена: {price}")
